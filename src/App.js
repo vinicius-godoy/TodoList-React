242b9ff6a2f761b07a01ff145b47b6694e05a56d
Vinícius Rodrigues Godoy dos Santos <v.godoyrodrigues@gmail.com>
2021-07-15 12:14:49 -0300

    Adiciona useEffect para salvar os Todos

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './TodoList'
 import uuidv4 from 'uuid/v4'
 
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
+
 function App() {
   const [todos, setTodos] = useState([{ id: 1, name: 'Todo 1', complete: false}])
   const todoNameRef = useRef()
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if(storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
 
   function handleAddTodo(e) {
     const name = todoNameRef.current.value
