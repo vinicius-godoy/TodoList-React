9160fb285708b0af70e9d0646876b70b78bf2d49
Vinícius Rodrigues Godoy dos Santos <v.godoyrodrigues@gmail.com>
2021-07-15 11:17:22 -0300

    Cria componente TodoList e cria estrutura do App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import TodoList from './TodoList'
 
 function App() {
-  return null
+  return (
+    <>
+    <TodoList />
+    <input type="text" />
+    <button>Add Todo</button>
+    <button>Clear Todos</button>
+    <div>0 left to do</div>
+    </>
+  )
 }
 
 export default App;
