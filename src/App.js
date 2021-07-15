2669877874063dcaadfe24004377e26a69716454
Vinícius Rodrigues Godoy dos Santos <v.godoyrodrigues@gmail.com>
2021-07-15 12:12:07 -0300

    Cria onClick para o botão de Adicionar Todo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React, { useState} from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './TodoList'
+import uuidv4 from 'uuid/v4'
 
 function App() {
   const [todos, setTodos] = useState([{ id: 1, name: 'Todo 1', complete: false}])
+  const todoNameRef = useRef()
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if(name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
+  }
+
   return (
     <>
     <TodoList todos={todos} />
-    <input type="text" />
-    <button>Add Todo</button>
+    <input type="text" ref={todoNameRef} />
+    <button onClick={handleAddTodo}>Add Todo</button>
     <button>Clear Todos</button>
     <div>0 left to do</div>
     </>
