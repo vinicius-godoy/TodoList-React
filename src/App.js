7ea023d7ac90be2f406e16db09869da7fd591669
Vinícius Rodrigues Godoy dos Santos <v.godoyrodrigues@gmail.com>
2021-07-15 11:55:41 -0300

    Cria Todo Component e Torna TodoList funcional

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState} from 'react';
 import TodoList from './TodoList'
 
 function App() {
+  const [todos, setTodos] = useState([{ id: 1, name: 'Todo 1', complete: false}])
   return (
     <>
-    <TodoList />
+    <TodoList todos={todos} />
     <input type="text" />
     <button>Add Todo</button>
     <button>Clear Todos</button>
